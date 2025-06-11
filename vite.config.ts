8adfaa3127c945c397140e745db33aae886596c9
Goodluck-dc14 <i.ogbonna@alustudent.com>
2025-06-11 04:31:17 +0200

    bug fix/codesandbox error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false,
+    allowedHosts: ['all'],
+    hmr: {
+      clientPort: 443,
+    },
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    strictPort: false,
   },
 })
